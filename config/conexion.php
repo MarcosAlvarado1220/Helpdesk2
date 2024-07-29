e00ce5a00af63150b142db4dc0a0fa0c8f1e935d
MarcosAlvarado1220 <83664975+MarcosAlvarado1220@users.noreply.github.com>
2024-07-29 07:56:55 -0600

    Add files via upload

---
+++ config/conexion.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+            try {
+				$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=avina_helpdesk_2","root","");
+				return $conectar;	
+			} catch (Exception $e) {
+				print "¡Error BD!: " . $e->getMessage() . "<br/>";
+				die();	
+			}
+        }
+
+        public function set_names(){	
+			return $this->dbh->query("SET NAMES 'utf8'");
+        }
+        
+        public static function ruta(){
+            return "http://localhost/hp_1/";
+			#return "http://192.168.9.1/help_desk/";
+		}
+
+    }
+?>
